f2a017605027b18284bc34bd9a0f5345db5b3dd8
Pau <pauserratosa5@gmail.com>
2020-11-08 15:20:21 +0100

    Descomentar Swagger Config.

--- src/main/java/com/webApp/webApp/configuration/SwaggConfiguration.java
+++ src/main/java/com/webApp/webApp/configuration/SwaggConfiguration.java
@@ -9,7 +9,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-/*
+
 @Configuration
 @EnableSwagger2
 //Swagger sirve para testear nuestros servicios Rest
@@ -21,4 +21,4 @@ public class SwaggConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("com.webApp.webApp.controller"))
                 .paths(PathSelectors.any()).build();
     }
-}*/
+}
