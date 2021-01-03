02468de175e5c85e596186fce186c87d056e985f
Pau <pauserratosa5@gmail.com>
2021-01-03 12:56:49 +0100

    Añadir httpbasic() para basic auth.

--- src/main/java/com/webApp/webApp/security/SecurityConfiguration.java
+++ src/main/java/com/webApp/webApp/security/SecurityConfiguration.java
@@ -22,7 +22,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
                 .antMatchers("/getUsers").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/**").hasRole("ADMIN")
                 .and().formLogin();
